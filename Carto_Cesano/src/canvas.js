3442783f11daf3ded37b5ff08d2a3409a9690cb0
Marina-Csn <marina.cesano@gmail.com>
2021-02-09 12:12:25 +0100

    Rendu devoir

---
+++ Carto_Cesano/src/canvas.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  var canvas = document.getElementById('canvas') ;
+  var ctx = canvas.getContext('2d');
+  var boussole = new Image();
+  var aiguille = new Image() ;
+  boussole.src = './images/compass.png' ;
+  aiguille.src = './images/needle.png' ;
+
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      moveNeedle(orientation);
+    }) ;
+  }
+
+  function moveNeedle(orientation) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(boussole, 0, 0);
+    ctx.save();
+    ctx.translate(100, 100);
+    ctx.rotate(orientation.alpha * (Math.PI / 180));
+    ctx.drawImage(aiguille, -100, -100);
+    ctx.restore();
+  }
+
+}) ;
